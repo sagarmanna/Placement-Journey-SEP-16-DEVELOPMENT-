aed2d59e542a904c64a009f446dbe62e5430265a
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-18 00:10:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ console.log("a");
 var a=2;
 a=5;
 console.log(a);
+
+
+
+let firstName = "SAGAR";
+let age = 21;
+let ismarred = false;
+console.log("this person namew is : "+firstName+"and their age is : "+age);
+console.log("this persons name is ");
+console.log(firstName);
