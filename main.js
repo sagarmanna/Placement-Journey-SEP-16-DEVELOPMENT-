9b7681d8287604f40092e272f6eb0f74f416e089
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-18 00:44:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//change variable
 console.log("Hello World ");
 console.log("a");
 
@@ -6,10 +8,32 @@ a=5;
 console.log(a);
 
 
-
+// Data Type 
 let firstName = "SAGAR";
 let age = 21;
 let ismarred = false;
 console.log("this person namew is : "+firstName+"and their age is : "+age);
 console.log("this persons name is ");
 console.log(firstName);
+
+//data type 
+//Loop in JS 
+let ans=0;
+for(let i=0;i<=1000;i++){
+    ans+=1;
+}
+console.log("Ans is : "+ans);
+
+
+let person1 = "D.Manna";
+let person2 = "M.Manna";
+let person3 = "R.Manna";
+const personArray = ["D.Manna", "M.Manna","R.Manna"];
+console.log(personArray);
+console.log(personArray);
+console.log(personArray);
+
+
+
+
+
