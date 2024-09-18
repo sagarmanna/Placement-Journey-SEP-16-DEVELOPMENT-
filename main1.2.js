c1041409aa29d58d2ec3af90edb56916fabe0492
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-18 21:07:59 +0530

    Add files via upload

--- main1.2.js
+++ main1.2.js
@@ -3,3 +3,28 @@
 let ans = 0;
 ans=ans+1+2+3+4+5+6;
 console.log(ans);
+
+let answer=0;
+for(let  i=0;i<10;i++){
+    answer=answer+1;
+console.log(answer);
+}
+
+
+let person1 = "Harkirat ";
+let person2 = "Raman";
+let person3 = "Kirat ";
+
+const personArray = ["Harkirat", "Raman","Kirat"];
+console.log(personArray[0]);
+console.log(personArray[1]);
+console.log(personArray[2]);
+
+
+
+const ages = [21,22,23,24,25];
+for(let i=0;i<ages.length;i++){
+    if(ages[i]%2==0){
+        console.log(ages[i]);
+    }
+}
