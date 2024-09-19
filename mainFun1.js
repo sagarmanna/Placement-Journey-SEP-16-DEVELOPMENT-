90d164e8f664e34ed4d16554c871197f2d527acf
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-19 21:24:20 +0530

    Add files via upload

---
+++ mainFun1.js
@@ -0,0 +1,8 @@
+const str = "Hello";
+
+str.replace(str,"world","javascript");
+console.log(str.replace(str,));
+
+const value ="hi my name is SAGAR";
+const words = value.split(" ");
+console.log(words);
