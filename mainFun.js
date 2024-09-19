4b994a8a0b2579336ce8e9b07a1aeb4309b94b92
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-19 18:40:33 +0530

    Add files via upload

--- mainFun.js
+++ mainFun.js
@@ -4,3 +4,19 @@ function sum(a,b){
 }
 const finalsum = sum(3,4);
 console.log(finalsum);
+
+//length of string
+
+const str="SAGAR";
+console.log(str.length);
+
+// indexof
+
+function findIndexof(str,target){
+    console.log("Orginal String : ", str);
+    console.log("index: ", str.indexOf(target));
+}
+findIndexof("Hello Sagar Helo SAGAR");
+
+str.length;
+str.lengthIndexOf;
