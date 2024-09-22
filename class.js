f9a9abbccda74c17e924adb8c7c32754cf9d8f77
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-22 10:45:54 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,16 @@
+class Animal{
+    constructor(name,legCount,speaks){
+        this.name=name;
+        this.legCount=legCount;
+        this.speaks= speaks;
+    }
+    
+    }
+
+
+let dog1 ={
+    name:"dog",
+    legCount: 4,
+    speaks:"bhow bhow",//dont do this 
+}
+let dog=new Animal("dog","bhow bhow");//do this 
