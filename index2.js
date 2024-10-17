d0d92fd76a4510b0e4be863118d8dca6ccf8070a
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-10-17 23:53:42 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,4 +1,27 @@
-// why nees fs library 
 
-// fs -> files system
+// creating an http server
+// express
+// node default library 
+
+const express=require("express");
+
+function(n){
+    let ans =0;
+    for(let i=1;i<=n;i++{
+        ans+=i;
+    }
+    return ans;
+}   
+const app=express();
+
+app.get("/",function(req,res){
+    const n=req.query.n;
+    res.send("hi there");
+})
+app.listen(3000);
+
+
+
+
+
 
