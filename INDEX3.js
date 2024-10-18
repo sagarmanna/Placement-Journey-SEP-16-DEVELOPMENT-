b161a7ad9875848b25e815015407ce9908c2915b
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-10-19 00:58:15 +0530

    Add files via upload

--- INDEX3.js
+++ INDEX3.js
@@ -1,10 +1,35 @@
-var users = [{
-   name: "Sagar",
-   metadata:{
-    profilespicture:"",
-    pronouns:"he/him",
-    
+// var users = [{
+//    name: "Sagar",
+//    metadata:{
+//     profilespicture:"",
+//     pronouns:"he/him",
 
-   }
 
+//    }
+
+// }]
+
+const express = require("express");
+const app =express();
+const users = [{
+    name:"Manna",
+    kidneys: [{
+        healthy:false
+    }]
 }]
+app.get("/", function (req ,res){
+    //write logic
+    const Manna=users[0].kidneys;
+    const numberOfKidneys= kidneys.length;
+    let numberOfKidneys=0;
+    for(leti=0;i<Manna.length;i++){
+    for(Manna[i].healthy){
+        numberOfKidneys=numberOfKidneys+1;
+    }
+
+}
+const numberOfKidneys=numberOfKidneys-numberOfKidneys;
+res.json{
+    
+}
+    })
