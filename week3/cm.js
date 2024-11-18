0bd973332d0e05cb4a9dc1dc8812d91920540b38
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-11-18 23:21:14 +0530

    Add files via upload

---
+++ week3/cm.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Home Page")
+})
+
+app.get("/users", (req, res) => {
+  res.send("Users Page")
+})
+
+app.listen(3000, () => console.log("Server Started"))
--- week3/m.js
+++ week3/m.js
@@ -2,23 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health- checkup", function(req,res){
-    const username = req.headers.username;
-    const passward = req.headers.passward;
-    const kidneyId = req.query.kidneyid;
 
-    if(username === "sagar" && passward === "pass"){
-        if(kidneyId==1 || kidneyId==2){
-        //do something with kidney here 
-        res.json({
-            msg: "your kidney is fine!"
-        })
-    } else {
-        res.json({
-            msg: "bad input"
-        })
-    }
-}
-
+res.status.json({"msg": "somethings up with your inputs"})
 });
-app.listen(3000);
